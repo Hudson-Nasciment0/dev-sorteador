545015fe6aee02685df678a65b08f4036da6655b
Hudson-Nasciment0 <nascimentoh714@gmail.com>
2024-06-27 21:48:48 -0300

    foi colocado if e elf para comparar valores de inputs e alert

--- scrip.js
+++ scrip.js
@@ -1,12 +1,19 @@
 const sort = document.querySelector(".button-sortear").addEventListener("click", () =>{
     const min = Math.ceil(parseInt(document.querySelector("#min").value))
     const max = Math.floor(parseInt(document.querySelector("#max").value))
-    const result = Math.floor(Math.random() * (max - min + 1)) + min 
+     
    
    
-   alert(result)
+   
 
-    
+   if(min < max){
+    const result = Math.floor(Math.random() * (max - min + 1)) + min
+    alert(result)
+   }
+
+   else{
+    alert("Você deve colocar o valor minímo menor que o valor máximo!")
+   }
 
 } ) 
 
